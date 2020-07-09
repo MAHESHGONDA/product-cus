57fc3ade4fd60eba0cfadc31177506bef5c1f1fd
MAHESHGONDA <maheshmalknoor@gmail.com>
2020-07-09 09:39:11 +0530

    Pagination

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,20 +1,47 @@
 import React, { Component } from "react";
 import OData from "react-odata";
 import Product from "./Product";
+import Pagination from "./Pagination";
 
 class Products extends Component {
-  state = {};
+  constructor(props) {
+    super(props);
+    this.count = 0;
+  }
+  state = {
+    productsCount: 11,
+    top: 5,
+    skip: 0,
+    activePage: 1,
+    productsPerPage: 5,
+  };
   render() {
+    const { top, skip } = this.state;
+
     const baseUrl =
-      "https://services.odata.org/Experimental/OData/(S(l3vkrglmrsqth3tkd51sxm0q))/OData.svc/Products?$format=json";
+      "https://services.odata.org/Experimental/OData/(S(l3vkrglmrsqth3tkd51sxm0q))/OData.svc/Products?$top=" +
+      top +
+      "&$skip=" +
+      skip +
+      "&$format=json";
+
     // https://services.odata.org/V2/Northwind/Northwind.svc/Products?$format=json
     // const baseUrl = "http://services.odata.org/V4/TripPinService/People";
     // const query = { filter: { FirstName: "Russell" } };
+
+    const countUrl =
+      "https://services.odata.org/Experimental/OData/(S(l3vkrglmrsqth3tkd51sxm0q))/OData.svc/Products/$count";
     return (
       //const query = { filter: { FirstName: 'Russell' } };
       <React.Fragment>
         <div className="">
-          <h2>Products</h2>
+          <div>
+            <OData baseUrl={countUrl}>
+              {({ loading, error, data }) =>
+                this.displayCount(loading, error, data)
+              }
+            </OData>
+          </div>
         </div>
         <div className="table-responsive table-wrapper-scroll-y my-custom-scrollbar tableFixHead">
           <table className="table">
@@ -28,19 +55,52 @@ class Products extends Component {
             </thead>
             <tbody>
               <OData baseUrl={baseUrl}>
-                {({ loading, error, data }) =>
-                  this.onProductCallBack(loading, error, data)
+                {
+                  ({ loading, error, data }) =>
+                    this.onProductCallBack(loading, error, data)
+
+                  //
                 }
               </OData>
             </tbody>
           </table>
+          <Pagination
+            productsCount={this.state.productsCount}
+            productsPerPage={this.state.productsPerPage}
+            activePage={this.state.activePage}
+            onPageChange={this.onPageChange}
+          />
         </div>
       </React.Fragment>
     );
   }
+  onPageChange = (pageNo) => {
+    console.log("page change:", pageNo);
+    // this.setState({ activePage: pageNo });
+    const top = this.state.productsPerPage * pageNo;
+    let skip = top - this.state.productsPerPage;
+    // if (skip < 0) {
+    //   skip = 0;
+    // }
+    this.setState({
+      top: top,
+      skip: skip,
+      activePage: pageNo,
+    });
+  };
   componentDidMount() {
-    //
+    debugger;
   }
+  displayCount = (loading, error, data) => {
+    //
+    if (data) {
+      debugger;
+      this.count = parseInt(new TextDecoder("utf-8").decode(data));
+      // this.setState({productsCount:count});
+      const text = "Products(" + this.count + ")";
+      return <span>{text}</span>;
+    }
+  };
   onProductCallBack = (loading, error, data) => {
     console.log("products call back :");
 
@@ -48,7 +108,9 @@ class Products extends Component {
       console.log("Error in loading Products", error);
     }
     if (data) {
-      //data
+      if (data.value.length > this.state.productsPerPage) {
+        data.value.pop();
+      }
       return data.value.map((product) => {
         return <Product key={product.ID} product={product}></Product>;
       });
