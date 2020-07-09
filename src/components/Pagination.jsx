4dd8b9cbd2986464494bc781e2b3cfaa1c6dea44
MAHESHGONDA <maheshmalknoor@gmail.com>
2020-07-09 09:41:18 +0530

    Pagination

---
+++ src/components/Pagination.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import _ from "lodash";
+
+const Pagination = (props) => {
+  const { productsCount, productsPerPage, onPageChange, activePage } = props;
+  var pages = Math.ceil(productsCount / productsPerPage);
+  if (pages === 1) return null;
+  const aPages = _.range(1, pages + 1);
+  debugger;
+  return (
+    <nav aria-label="Page navigation example">
+      <ul className="pagination">
+        {aPages.map((page) => {
+          console.log(page);
+          return (
+            <li
+              className={page == activePage ? "page-item active" : "page-item"}
+            >
+              <a
+                role="button"
+                className="page-link"
+                onClick={() => onPageChange(page)}
+              >
+                {page}
+              </a>
+            </li>
+          );
+        })}
+      </ul>
+    </nav>
+  );
+};
+
+export default Pagination;
